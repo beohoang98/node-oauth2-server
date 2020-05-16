fcf2c1835c4d048744c41f718ded638850755b2f
Beo Hoang <anaizhu98@gmail.com>
2020-05-16 12:18:12 +0700

    include dist

---
+++ dist/lib/interfaces/index.d.ts
@@ -0,0 +1,6 @@
+export { AuthorizationCode } from './authorization-code.interface';
+export { Client } from './client.interface';
+export * from './model.interface';
+export { RefreshToken } from './refresh-token.interface';
+export { Token } from './token.interface';
+export { User } from './user.interface';
